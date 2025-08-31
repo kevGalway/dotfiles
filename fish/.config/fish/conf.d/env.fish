fde36f14869cdba92e6ebebc7f3069d24c791977
Kevin Farrell <kev9229@gmail.com>
2025-08-31 00:23:30 -0600

    bug fixes with aws path

--- fish/.config/fish/conf.d/env.fish
+++ fish/.config/fish/conf.d/env.fish
@@ -1,7 +1,7 @@
 set -x EDITOR nvim
 set -x SUDO_EDITOR $EDITOR
 set -x BAT_THEME ansi
-fish_add_path -g ~/.local/bin ./bin
+fish_add_path -g ~/bin ~/.local/bin
 
 set -gx LESS -R
 set -gx MANPAGER "sh -c 'col -bx | bat -l man -p'"
